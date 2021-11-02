1ba674838f1d526c5897b7c8dd7967db9235c09b
Marcos Nolasco <marcosabn@gmail.com>
2021-11-02 15:17:22 -0300

    simpler apis created

---
+++ openFood/foodData/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from foodData.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'code',
+            'url',
+            'creator',
+            'created_t',
+            'last_modified_t',
+            'product_name',
+            'generic_name',
+            'quantity'
+        )
--- openFood/foodData/views.py
+++ openFood/foodData/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from foodData.models import Product
+from foodData.serializers import ProductSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-# Create your views here.
+
+class ProductList(mixins.ListModelMixin,
+                  generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
